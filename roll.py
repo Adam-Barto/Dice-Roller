677eaf604f2bb7f00be8610cd84b2bc145ee38ab
Adam-Barto <adam.jbarto@gmail.com>
2023-03-14 14:54:49 -0400

    Dice Roller Baseline made

--- roll.py
+++ roll.py
@@ -4,4 +4,35 @@
 # roll 2 - produce two numbers 1-6 and print them
 # roll 3 - produce three numbers and print them.
 #
+import random
+import sys
+
+
+def die():
+    return random.randint(1, 6)
+
+
+# print(die())
+
+number_of_times = 1
+
+val = sys.argv
+
+
+def roll():
+    print(die())
+
+
+def rolln(number):
+    print_this = ""
+    while number > 0:
+        print_this.join(str(die()))
+        number = number - 1
+    print(print_this)
+
+
+if len(val) == 2:
+    rolln(int(val[1]))
+else:
+    roll()
 
